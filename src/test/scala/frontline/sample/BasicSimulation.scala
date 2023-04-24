0b63e7a2197af4f39c200099b5b703eb386b8e8a
juliusmartin.juane <juliusmartin.juane@immowelt.de>
2023-04-24 14:37:04 +0200

    key change

--- src/test/scala/frontline/sample/BasicSimulation.scala
+++ src/test/scala/frontline/sample/BasicSimulation.scala
@@ -26,9 +26,9 @@ class BasicSimulation extends Simulation {
 
   val devQueueUrl = "sqs_classified_events-republication-detection-stage"
   val endpoint = "https://sqs.eu-west-1.amazonaws.com"
-  val accessKey = "ASIA5QZRJFFBMSHU56X7"
-  val secretKey = "z9s1d0PTBvsbawGUfAVliB7JhLV2BaH1xkA/+Tzl"
-  val session_token = "IQoJb3JpZ2luX2VjEPT//////////wEaCWV1LXdlc3QtMSJHMEUCIQC6EbI6bc4AJmOAOJr6G7+4z0Np020mdOEPLIYqn7EbCwIgbyO/l4tEjMvZvriVlvm3ctqATpyQlzEUa0GnkUFcckoqvwMI7f//////////ARAAGgw5Mjk0MjY5MDk1MDYiDLoVJbYbA+WJKsLt8yqTA/PuhXWLjfR6yZAcI3ZjYEETLMUGgM+NhiDXKXYkK2xWBz5WhWm/Fymd7tLuvctu4+v6ssl27SkazMt29yDhBTOCr6QXpVWO/qv7cOqjHs0e2Hi4U5YW3UKuqNCSW/t1GlD677MkpP4LvCAb+ug7ntonqrWuGqqE1ahjkpEP8iL8PC0sqlr36cqYHxAgcPEaVchWVILBYS7/UIa8c45DVSY9lKZi/8rBmzJWFwwAHPjbMyayYvYCM0X5MfWZ09lPD2FodXPs4jdvw53cUNOuQDYXM4EqOSCv4p9smxbzocHmXTOHXBxiX2DEClyqjBh6NNjbbq3ta3L+JM8fNwIiZELCGSUEQh7mgH+wRK1asAv9rWHAhL/g85H85FyP/wnSJEMTuVTw9ZPGV2rrp7dVR/f5eDUdrTce5f+0qHZeV81ISZRMhRlC32plHDKgTFlWH+Yugi9VFPkXtkFCwvutqyMMwEMGoe8i+0ATNtWQtN1CEA3IKwYCWUJojclRNf3NrKYVPodpra+F82HYdpH7omFQyeEwudOZogY6pgEukOyLRuPTh9tC+Zqg0uM+stQmHUUU1+givGheenRYfmwXDurj4MAPe29WyKpo/7MphbBZNVrlAcL8bs/NjkVRNKgpnQcXso5amjwFD4mXa5Pn1cCii7VBxugk0XVt2Wux5f9NSZrPVWQRg8NLEC9MtdeDXvc/yFA2quJugrEgTL2zzQLH+rH4+qus7Lq8SFrP+iSI2snHRsXaIC69sgD1D+v+wayY"
+  val accessKey = "xxx"
+  val secretKey = "xxx"
+  val session_token = "xxx"
   val batchSize = 10
   val awsCreds = new BasicSessionCredentials(accessKey, secretKey, session_token)
   var client: AmazonSQS = AmazonSQSClientBuilder
